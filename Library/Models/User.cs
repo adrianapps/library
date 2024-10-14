ff63a55674351dfd55049f44c57b3025b79123d6
tempestpulse <bulsar@tutanota.com>
2024-10-14 15:54:19 +0200

    initialize List<Borrow> on creation

--- Library/Models/User.cs
+++ Library/Models/User.cs
@@ -7,5 +7,5 @@ public class User : IdentityUser
     public string FirstName { get; set; }
     public string LastName { get; set; }
 
-    public ICollection<Borrow> Borrows { get; set; }
+    public ICollection<Borrow> Borrows { get; set; } = new List<Borrow>();
 }
