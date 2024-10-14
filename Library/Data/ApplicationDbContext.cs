f3b17e814d636578af4827a9e78e8ddc96599ec7
tempestpulse <bulsar@tutanota.com>
2024-10-14 15:52:08 +0200

    add DbSets for models

--- Library/Data/ApplicationDbContext.cs
+++ Library/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Library.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Library.Data;
 
@@ -8,5 +10,27 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+
+    public DbSet<Book> Books { get; set; }
+    public DbSet<BookFile> BookFiles { get; set; }
+    public DbSet<Borrow> Borrows { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Borrow>()
+            .HasOne(b => b.Book)
+            .WithMany(bk => bk.Borrows)
+            .HasForeignKey(b => b.BookId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Borrow>()
+            .HasOne(b => b.User)
+            .WithMany(u => u.Borrows)
+            .HasForeignKey(b => b.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
